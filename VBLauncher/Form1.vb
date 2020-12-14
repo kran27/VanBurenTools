f0d5d4a418f8ba81863e01f2ff815023cb5be747
Kranberri <56488228+kran27@users.noreply.github.com>
2020-12-13 21:12:53 -0800

    Automatically Load New .WEA and .CRT
    I hex-edited new CRTs and WEAs for anything that didn't have them, and now it will load those files. the weapons ones are being worked on but can be forced into your inventory if you edit Default_PC.CRT, but so far I can't get any of them to be equipped.

--- VBLauncher/Form1.vb
+++ VBLauncher/Form1.vb
@@ -1,4 +1,35 @@
 ﻿Public Class Form1
+    Public undir As String = Application.StartupPath & "\Override"
+    Private Sub LoadExtras() Handles MyBase.VisibleChanged
+        If Not IO.Directory.Exists(undir) Then
+            IO.Directory.CreateDirectory(undir & "\_CRT")
+            IO.Directory.CreateDirectory(undir & "\_WEA")
+            IO.Directory.CreateDirectory(undir & "\Critters")
+            IO.Directory.CreateDirectory(undir & "\Interface")
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersAnt.CRT", My.Resources.CrittersAnt)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersAntQ.CRT", My.Resources.CrittersAntQ)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersBat.CRT", My.Resources.CrittersBat)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersBeetle.CRT", My.Resources.CrittersBeetle)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersCentipede.CRT", My.Resources.CrittersCentipede)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersDeathclaw.CRT", My.Resources.CrittersDeathclaw)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersDesertStalker.CRT", My.Resources.CrittersDesertStalker)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersGila.CRT", My.Resources.CrittersGila)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersManTrap.CRT", My.Resources.CrittersManTrap)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersRadToad.CRT", My.Resources.CrittersRadToad)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersRat.CRT", My.Resources.CrittersRat)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersThornSlinger.CRT", My.Resources.CrittersThornSlinger)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersTiger.CRT", My.Resources.CrittersTiger)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersWaspGiant.CRT", My.Resources.CrittersWaspGiant)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersWeedling.CRT", My.Resources.CrittersWeedling)
+            IO.File.WriteAllBytes(undir & "\_CRT\CrittersWolf.CRT", My.Resources.CrittersWolf)
+            IO.File.WriteAllBytes(undir & "\_WEA\Weapons2mmGaussPistol.WEA", My.Resources.Weapons2mmGaussPistol)
+            IO.File.WriteAllBytes(undir & "\_WEA\WeaponsLaserSaw.WEA", My.Resources.WeaponsLaserSaw)
+            IO.File.WriteAllBytes(undir & "\_WEA\WeaponsSawedOffShotgun.WEA", My.Resources.WeaponsSawedOffShotgun)
+            IO.File.WriteAllBytes(undir & "\Critters\CR_DesertStalk.B3D", My.Resources.CR_DesertStalk)
+            IO.File.WriteAllBytes(undir & "\Interface\WP_2mmGaussPistol_ACT.TGA", My.Resources.WP_2mmGaussPistol_ACT)
+            IO.File.WriteAllBytes(undir & "\Interface\WP_2mmGaussPistol_INV.TGA", My.Resources.WP_2mmGaussPistol_INV)
+        End If
+    End Sub
 #Region " Move Form "
     Public MoveForm As Boolean
     Public MoveForm_MousePosition As Point
