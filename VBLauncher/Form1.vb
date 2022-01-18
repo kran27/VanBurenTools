7f57d38ef30b87727fb2dc73f8c853c25eef7d9f
Kranberri <56488228+kran27@users.noreply.github.com>
2022-01-17 23:40:40 -0800

    Automatically delete None.CRT
    None.CRT is the default named character file, that can be annoying to have to confirm deleteion of every time you open the game.

--- VBLauncher/Form1.vb
+++ VBLauncher/Form1.vb
@@ -1,7 +1,27 @@
 ﻿Imports System.IO.Compression
 Public Class Form1
     Public undir As String = Application.StartupPath & "\Override\UnusedThings"
-    Private Sub RandomBackground() Handles MyBase.Load
+#Region " Move Form "
+    Public MoveForm As Boolean
+    Public MoveForm_MousePosition As Point
+    Public Sub MoveForm_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown, PictureBox1.MouseDown, PictureBox2.MouseDown, PictureBox3.MouseDown, PictureBox4.MouseDown, PictureBox5.MouseDown
+        If e.Button = MouseButtons.Left Then
+            MoveForm = True
+            MoveForm_MousePosition = e.Location
+        End If
+    End Sub
+    Public Sub MoveForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove, PictureBox1.MouseMove, PictureBox2.MouseMove, PictureBox3.MouseMove, PictureBox4.MouseMove, PictureBox5.MouseMove
+        If MoveForm Then
+            Me.Location = Me.Location + (e.Location - MoveForm_MousePosition)
+        End If
+    End Sub
+    Public Sub MoveForm_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp, PictureBox1.MouseUp, PictureBox2.MouseUp, PictureBox3.MouseUp, PictureBox4.MouseUp, PictureBox5.MouseUp
+        If e.Button = MouseButtons.Left Then
+            MoveForm = False
+        End If
+    End Sub
+#End Region
+    Private Sub Startup() Handles MyBase.Load
         Dim random As New Random
         Dim rand As String = random.Next(1, 9)
         PictureBox4.Parent = PictureBox5
@@ -25,28 +45,6 @@ Public Class Form1
             PictureBox5.Image = My.Resources.BG9
         End If
         PictureBox5.SizeMode = PictureBoxSizeMode.StretchImage
-    End Sub
-#Region " Move Form "
-    Public MoveForm As Boolean
-    Public MoveForm_MousePosition As Point
-    Public Sub MoveForm_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown, PictureBox1.MouseDown, PictureBox2.MouseDown, PictureBox3.MouseDown, PictureBox4.MouseDown, PictureBox5.MouseDown
-        If e.Button = MouseButtons.Left Then
-            MoveForm = True
-            MoveForm_MousePosition = e.Location
-        End If
-    End Sub
-    Public Sub MoveForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove, PictureBox1.MouseMove, PictureBox2.MouseMove, PictureBox3.MouseMove, PictureBox4.MouseMove, PictureBox5.MouseMove
-        If MoveForm Then
-            Me.Location = Me.Location + (e.Location - MoveForm_MousePosition)
-        End If
-    End Sub
-    Public Sub MoveForm_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp, PictureBox1.MouseUp, PictureBox2.MouseUp, PictureBox3.MouseUp, PictureBox4.MouseUp, PictureBox5.MouseUp
-        If e.Button = MouseButtons.Left Then
-            MoveForm = False
-        End If
-    End Sub
-#End Region
-    Private Sub Warning() Handles MyBase.VisibleChanged
         If Not IO.File.Exists(Application.StartupPath & "\F3.exe") Then
             MsgBox("Please put the launcher in the same directory as the game so you can launch it!", MsgBoxStyle.Critical Or MsgBoxStyle.OkOnly, "Game Executable Not Found!")
         ElseIf IO.Directory.Exists(undir) Then
@@ -58,6 +56,9 @@ ExtractZip:
             IO.File.WriteAllBytes(undir & "\UnusedThings.zip", My.Resources.UnusedThings)
             IO.Compression.ZipFile.ExtractToDirectory(undir & "\UnusedThings.zip", undir)
             IO.File.Delete(undir & "\UnusedThings.zip")
+        End If
+        If IO.File.Exists(My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\F3\Characters\None.CRT") Then
+            IO.File.Delete(My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\F3\Characters\None.CRT")
         End If
     End Sub
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
